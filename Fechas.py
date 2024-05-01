02aaa1c8c22aad7514a71869f70b07570dfb1c84
Juan319-u <80417972+Juan319-u@users.noreply.github.com>
2024-04-30 22:22:35 -0500

    Update Fechas.py

--- Fechas.py
+++ Fechas.py
@@ -9,28 +9,21 @@ Ultima Actualizacion 28/04/2024
 from datetime import date,timedelta
 class fechas():
     fecha_inicial=date
-    fecha_final=date
-
-    def fecha_inicial(self):
+    def Fecha_inicial(self,dias_prestamo):
             while True:
                 dia=int(input("ingresa el dia "))
                 mes=int(input("ingresa el mes "))
                 año=int(input("ingresa el año "))
                 if 1 <= mes <= 12 and 1 <= dia <= 31 and año >= 0:
                     self.fecha_inicial=date(año,mes,dia)
-                    break   
+                    break
                 else:
                     print("Error de fecha")
 
-    def calcular_fecha_final(self):
-        dias_prestamo = int(input("¿Cuántos días se le prestará? "))
-        if dias_prestamo >= 0:
-            fecha_final = self.fecha_inicial + timedelta(days=dias_prestamo)
-            self.fecha_inicial=fecha_final
-            print(fecha_final)
-        else:
-            print("Error en la cantidad de dias")
-
-p=fechas()
-p.fecha_inicial()
-p.calcular_fecha_final()
+            if dias_prestamo >= 0:
+                fecha_final = self.fecha_inicial + timedelta(days=dias_prestamo)
+                fecha_final=str(fecha_final)
+            else:
+                print("Error en la cantidad de dias")
+            return fecha_final
+       
